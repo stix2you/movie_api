dbbe82df6c4bb9a94d56477eeec2dc87b3a9cda1
Doug Hill <stix2you@gmail.com>
2024-02-18 16:28:49 -0600

    incorporate hashing into Passport username/password verification

--- passport.js
+++ passport.js
@@ -11,22 +11,26 @@ let Users = Models.User,
 
 // takes username and password from the request body and uses Mongoose to check the database for a user with the same username
 passport.use(
-    new LocalStrategy(
+    new LocalStrategy( // this is a new instance of the LocalStrategy
         {
             usernameField: 'username',
             passwordField: 'password',
         },
         
         async (username, password, callback) => {
-            console.log('${username} ${password}');
-            await Users.findOne({ username: username })
-                .then((user) => {
-                    if (!user) {
+            console.log('${username} ${password}');    // logs the username and password to the console
+            await Users.findOne({ username: username })  // uses the findOne method to look for a user with the same username
+                .then((user) => {                  // if the user is found, the then method will be called
+                    if (!user) {  // if the user is not found, return a message
                         console.log('incorrect username');
                         return callback(null, false, {
                             message: 'Incorrect username or password.',
                         });
                     }
+                    if (!user.validatePassword(password)) {   // if the password is incorrect, return a message, validatePassword is a method defined in the models.js file
+                        console.log('incorrect password');
+                        return callback(null, false, { message: 'Incorrect password.' });   
+                    }      
                     console.log('finished');
                     return callback(null, user);
                 })
