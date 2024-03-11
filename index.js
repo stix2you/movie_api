d94d8e5fd08476f8131e483cf32fb7fa0f080af6
Doug Hill <stix2you@gmail.com>
2024-03-11 17:57:32 -0500

    remove update movie functionality

--- index.js
+++ index.js
@@ -80,34 +80,6 @@ app.get('/movies/:Title', passport.authenticate('jwt', { session: false }),
          });
    });
 
-// Update a single movie's details
-app.put('/movies/:_id', passport.authenticate('jwt', { session: false }),
-   async (req, res) => {
-      const movieId = req.params._id;  // Movie ID to update
-      console.log('movieId: ' + movieId);         // Log the movie ID to the console
-      const updateData = req.body; // Data to update
-      console.log('updateData: ' + JSON.stringify(updateData));   // Log the data to the console
-
-      try {
-         console.log(`Attempting to update movie with ID ${movieId} with data:`, JSON.stringify(updateData, null, 2));
-         const updatedMovie = await Movies.findByIdAndUpdate(
-            movieId,
-            { $set: updateData },
-            { new: true } // This option returns the document after update was applied.
-         );
-         console.log('updatedMovie: ' + updatedMovie);  // Log the updated movie to the console
-
-         if (!updatedMovie) {
-            res.status(404).send('Movie with the given ID was not found.');  // If the movie is not found, return a 404 response
-         } else {
-            res.json(updatedMovie);  // ...otherwise, return the updated movie
-         }
-      } catch (error) {
-         console.error('Error updating movie:', error);
-         res.status(500).send('Error: ' + error);
-      }
-   });
-
 // Return data about a genre by name of genre -- NEW
 app.get('/genres/:name', passport.authenticate('jwt', { session: false }),
    async (req, res) => {
